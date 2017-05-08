4c5fb0874e53122dd8a9a576aa2fec042ac529ae
www2t <keephome@163.com>
2017-05-08 17:10:20 +0800

    Update main.go

--- main.go
+++ main.go
@@ -2,5 +2,16 @@
 
 package main
 
+func initLoger() error {
+	file, err := os.OpenFile("./log.txt", os.O_WRONLY|os.O_APPEND|os.O_CREATE, os.ModeAppend)
+	if err != nil {
+		return err
+	} else {
+		log.SetFlags(log.LstdFlags | log.Lshortfile)
+		log.SetOutput(file)
+		return nil
+	}
+}
+
 func main(){
 }
