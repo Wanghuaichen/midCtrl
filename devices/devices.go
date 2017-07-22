448328cae405eb19b62e12c9146e2d731a58db6d
水上清风 <keephome@163.com>
2017-07-22 17:26:16 +0800

    上报状态时先判断设备是不存在

--- devices/devices.go
+++ devices/devices.go
@@ -351,10 +351,12 @@ func reportDevStatus() {
 }
 // hdid = 16 为喷淋
 func reportPenLinStatus(){
-	devState := make(url.Values)
-	devState["isOk"] = []string{strconv.FormatInt(int64((*devList[16]).isOk), 10)}
-	devState["state"] = []string{strconv.FormatInt(int64((*devList[16]).state), 10)}
-	sendData("设备状态", 16, devState)
+	if dev,ok:= devList[16]; ok{
+		devState := make(url.Values)
+		devState["isOk"] = []string{strconv.FormatInt(int64(dev.isOk), 10)}
+		devState["state"] = []string{strconv.FormatInt(int64(dev.state), 10)}
+		sendData("设备状态", 16, devState)
+	}
 }
 func sendData(urlStr string, id uint, data url.Values) {
 	var msg comm.MsgData
